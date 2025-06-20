86a4b37a8c5fa9295351482cb1a180b8c0bd89d8
Cihat YİGİT <agileswiftcargo@gmail.com>
2025-06-20 10:59:24 +0300

    versiyon1

---
+++ src/main/java/pages/CihatPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CihatPage extends BasePage {
+
+    // Constructor
+    public CihatPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    // Locators
+
+
+}
