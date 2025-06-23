e3f20e811f5822299629d75376fa59f3437c4b5a
Cihat YİGİT <agileswiftcargo@gmail.com>
2025-06-23 14:37:50 +0300

    cihat1

--- src/main/java/pages/CihatPage.java
+++ src/main/java/pages/CihatPage.java
@@ -1,16 +1,46 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CihatPage extends BasePage {
 
     // Constructor
     public CihatPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
+
     }
 
 
     // Locators
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[1]/div/h2")
+    public WebElement İnstructorText;
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[1]/a")
+    public WebElement AllİnstructorButon;
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[2]/div")
+    public WebElement İnstructorSlider;
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[2]/div/div[1]/div/div[8]/div/div/div/div[2]/a[1]/h3")
+    public WebElement İnstructorSliderisim;
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[2]/div/div[1]/div/div[8]/div/div/div/div[2]/div")
+    public WebElement İnstructorSliderBegeniPuanı;
+
+    @FindBy(xpath ="//*[@id=\"app\"]/section[15]/div[2]/div/div[1]/div/div[16]/div/div/div/div[2]/a[2]")
+    public WebElement ReserveaLifeMeeting;
+
+
+
+
+
+
+
 
 
 }
