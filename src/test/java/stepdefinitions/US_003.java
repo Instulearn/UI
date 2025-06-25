4203974aa327a701b570037355b685c8e8a82d56
filiz-tester <filizturkkan@gmail.com>
2025-06-25 21:53:49 +0300

    filizv6

--- src/test/java/stepdefinitions/US_003.java
+++ src/test/java/stepdefinitions/US_003.java
@@ -9,20 +9,25 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 import pages.BasePage;
+import pages.FilizPage;
 import pages.RegisterPage;
 import utils.JSUtilities;
 
 
 public class US_003 {
+WebDriver driver = DriverManager.getDriver();
+
 
     @Then("{string} başlığı görünür olmalı")
     public void baslikGorunurOlmali(String beklenenBaslik) {
 
 
-        DriverManager Driver;
-        WebElement baslikElementi = Driver.getDriver().findElement(
-                By.xpath("//h1[contains(text(),'" + beklenenBaslik + "')]")
-        );
+
+        WebElement baslikElementi = new FilizPage(driver).transformYourFutureTitle;
+
+
+
+
         Assert.assertTrue(baslikElementi.isDisplayed(), "Beklenen başlık görünür değil: " + beklenenBaslik);
     }
 
