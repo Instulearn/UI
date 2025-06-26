705745070254b0cc9a5035c3b0c10ecfd2a1e707
Ömer Can Yağcı <omercan.yagcii@gmail.com>
2025-06-26 11:31:58 +0300

    omerv4

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -16,8 +16,8 @@ import org.testng.annotations.BeforeClass;
 				"json:target/cucumber-reports/cucumber.json", // JSON raporu (Cucumber HTML için gerekli)
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" // Allure raporları için plugin
 		},
-		monochrome = false,                         // Konsol çıktısını daha okunabilir hale getirir
-		tags = "google"                           // Yalnızca belirtilen etiketli senaryolar çalıştırılır.
+		monochrome = true,                         // Konsol çıktısını daha okunabilir hale getirir
+		tags = "@google"                           // Yalnızca belirtilen etiketli senaryolar çalıştırılır.
 )
 public class TestRunner extends AbstractTestNGCucumberTests{
 	private static final Logger logger = LogManager.getLogger(TestRunner.class);
